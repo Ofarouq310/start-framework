5d6ce328c01682d420b382019399378f5d40e28b
Ofarouq310 <ofarouq310@gmail.com>
2025-01-11 21:31:48 +0200

    add project files

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+
+function About() {
+  return (
+    <section id="about" className="about">
+      <h2>About Us</h2>
+      <p>
+        Start Framework is your go-to solution for modern, responsive web design.
+        Whether you are building a portfolio, business website, or blog, we have you covered.
+      </p>
+    </section>
+  );
+}
+
+export default About;
